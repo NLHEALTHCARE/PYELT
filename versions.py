98a7854dae184f6440dd4e813675033fa6111f40
Henk-Jan van Reenen <henk-jan.van.reenen@nlhealthcareclinics.com>
2016-09-15 15:03:25 +0200

    package-build V0.9.4.3-alpha

--- versions.py
+++ versions.py
@@ -1,10 +1,8 @@
-current_version = ('0.9.4.1-alpha','hjvreenen','package-build V0.9.4.a0', 'b7221fde381f9355fdc03e780bdde4c268521fc3', '2016-09-15 14:02:18.738355')
-
-version_archive = [
-
+current_version = ('0.9.4.2-alpha','hjvreenen','package-build V0.9.4.2-alpha', '21cada33d4a6e6bc4da50f6900e0e8c1c07a57ee', '2016-09-15 15:01:50.184715')
+version_archive = [
     ('0.9.1.a0', 'hjvreenen', 'package-build V0.9.0.a0', '', '2016-09-15 11:08:42.594143')
 ,     ('0.9.2.a0', 'hjvreenen', 'package-build V0.9.2.a0', '976e8402c57c075435f5663a638604194ca2ba70', '2016-09-15 11:10:51.613949')
 ,     ('0.9.3.a0', 'hjvreenen', 'package-build V0.9.3.a0', 'b8fb67a9e9355edae5cd52164939c54f08ddadeb', '2016-09-15 13:14:12.130335')
-,     ('0.9.4.a0','hjvreenen','package-build V0.9.4.a0', 'b7221fde381f9355fdc03e780bdde4c268521fc3', '2016-09-15 14:02:18.738355')
-, ]
-
+,     ('0.9.4.a0', 'hjvreenen', 'package-build V0.9.4.a0', 'b7221fde381f9355fdc03e780bdde4c268521fc3', '2016-09-15 14:02:18.738355')
+,     ('0.9.4.2-alpha','hjvreenen','package-build V0.9.4.2-alpha', '21cada33d4a6e6bc4da50f6900e0e8c1c07a57ee', '2016-09-15 15:01:50.184715')
+, ]
