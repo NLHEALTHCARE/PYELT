313dfbd92e8da0a9089554e8fe3df4170ffc4712
Henk-Jan van Reenen <henk-jan.van.reenen@nlhealthcareclinics.com>
2016-09-15 11:24:29 +0200

    initial

--- versions.py
+++ versions.py
@@ -1,7 +1,5 @@
-current_version = ('0.9.1.a0','hjvreenen','package-build V0.9.1.a0', '2ee6924fbf6fa9c3f0c81c4e192e3db2d3599ef4', '2016-09-15 11:08:42.594143')
-
-version_archive = [
-
-    ('0.9.1.a0','hjvreenen','package-build V0.9.0.a0', '', '2016-09-15 11:08:42.594143')
-, ]
-
+current_version = ('0.9.2.a0','hjvreenen','package-build V0.9.2.a0', '976e8402c57c075435f5663a638604194ca2ba70', '2016-09-15 11:10:51.613949')
+version_archive = [
+    ('0.9.1.a0', 'hjvreenen', 'package-build V0.9.0.a0', '', '2016-09-15 11:08:42.594143')
+,     ('0.9.2.a0','hjvreenen','package-build V0.9.2.a0', '976e8402c57c075435f5663a638604194ca2ba70', '2016-09-15 11:10:51.613949')
+, ]
