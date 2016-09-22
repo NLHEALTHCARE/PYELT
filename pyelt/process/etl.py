bd6d53b970ee6dece35acb9179c3089a95edd6d1
RFWiegerinck <rob.wiegerinck@nlhealthcareclinics.com>
2016-09-22 15:26:04 +0200

    etl + dll weer oude versie

--- pyelt/process/etl.py
+++ pyelt/process/etl.py
@@ -756,8 +756,8 @@ AND hstg._valid AND {filter};""".format(
                       AND hstg._valid AND hstg._active
                       AND NOT EXISTS (SELECT 1 FROM {dv}._ref_valuesets sets WHERE sets.naam = hstg.{source_type_field});
 
-                    INSERT INTO {dv}._ref_values (_runid, _active, _source_system, _insert_date, _revision, valueset_oid, valueset_naam, code, weergave_naam, niveau, niveau_type, temp_id, temp_fk)
-                    SELECT DISTINCT {runid}, True, '{source_system}', now(), 0, {source_oid_field}, {source_type_field}, {source_code_field}, {source_descr_field}, {source_level_field}, {source_leveltype_field}, {source_tempid_field}, {source_tempfk_field}
+                    INSERT INTO {dv}._ref_values (_runid, _active, _source_system, _insert_date, _revision, valueset_oid, valueset_naam, code, weergave_naam, niveau, niveau_type
+                    SELECT DISTINCT {runid}, True, '{source_system}', now(), 0, {source_oid_field}, {source_type_field}, {source_code_field}, {source_descr_field}, {source_level_field}, {source_leveltype_field}
                     FROM {sor}.{sor_table} hstg
                     WHERE floor(hstg._runid) = floor({runid})
                       AND hstg._valid AND hstg._active
@@ -769,26 +769,26 @@ AND hstg._valid AND {filter};""".format(
                     SELECT {runid}, '{source_system}', now(), '{ref_type}', NULL
                     WHERE NOT EXISTS (SELECT 1 FROM {dv}._ref_valuesets sets WHERE sets.naam = '{ref_type}');
 
-                    INSERT INTO {dv}._ref_values (_runid, _active, _source_system, _insert_date, _revision, valueset_naam, code, weergave_naam, niveau, niveau_type, temp_id, temp_fk)
-                    SELECT DISTINCT {runid}, True, '{source_system}', now(), 0, '{ref_type}', {source_code_field}, NULL, NULL, NULL, NULL, NULL
+                    INSERT INTO {dv}._ref_values (_runid, _active, _source_system, _insert_date, _revision, valueset_naam, code, weergave_naam, niveau, niveau_type)
+                    SELECT DISTINCT {runid}, True, '{source_system}', now(), 0, '{ref_type}', {source_code_field}, NULL, NULL, NULL,
                     FROM {sor}.{sor_table} hstg
                     WHERE floor(hstg._runid) = floor({runid})
                       AND hstg._valid AND hstg._active
                       AND NOT EXISTS (SELECT 1 FROM {dv}._ref_values ref WHERE ref.valueset_naam = '{ref_type}' AND ref.code = hstg.{source_code_field}
-                                    AND ref.weergave_naam = hstg.{source_descr_field} AND ref.niveau = {source_level_field} AND ref.niveau_type = {source_leveltype_field) AND ref.temp_id = {source_tempid_field} AND ref.temp_fk = {source_tempfk_field};""".format(**params)
+                                    AND ref.weergave_naam = hstg.{source_descr_field} AND ref.niveau = {source_level_field} AND ref.niveau_type = {source_leveltype_field)};""".format(**params)
             else:
                 insert_sql = """
                     INSERT INTO {dv}._ref_valuesets (_runid, _source_system, _insert_date, naam, oid)
                     SELECT {runid}, '{source_system}', now(), '{ref_type}', NULL
                     WHERE NOT EXISTS (SELECT 1 FROM {dv}._ref_valuesets sets WHERE sets.naam = '{ref_type}');
 
-                    INSERT INTO {dv}._ref_values (_runid, _active, _source_system, _insert_date, _revision, valueset_naam, code, weergave_naam, niveau, niveau_type, temp_id, temp_fk))
-                    SELECT DISTINCT {runid}, True, '{source_system}', now(), 0, '{ref_type}', {source_code_field}, {source_descr_field}, {source_level_field}, {source_leveltype_field}, {source_tempid_field}, {source_tempfk_field}
+                    INSERT INTO {dv}._ref_values (_runid, _active, _source_system, _insert_date, _revision, valueset_naam, code, weergave_naam, niveau, niveau_type))
+                    SELECT DISTINCT {runid}, True, '{source_system}', now(), 0, '{ref_type}', {source_code_field}, {source_descr_field}, {source_level_field}, {source_leveltype_field}
                     FROM {sor}.{sor_table} hstg
                     WHERE floor(hstg._runid) = floor({runid})
                       AND hstg._valid AND hstg._active
                       AND NOT EXISTS (SELECT 1 FROM {dv}._ref_values ref WHERE ref.valueset_naam = '{ref_type}' AND ref.code = hstg.{source_code_field}
-                                    AND ref.weergave_naam = {source_descr_field} AND ref.niveau = {source_level_field} AND ref.niveau_type = {source_leveltype_field) AND ref.temp_id = {source_tempid_field} AND ref.temp_fk = {source_tempfk_field};""".format(**params)
+                                    AND ref.weergave_naam = {source_descr_field} AND ref.niveau = {source_level_field} AND ref.niveau_type = {source_leveltype_field);""".format(**params)
 
             self.execute(insert_sql, 'insert refs')
 
