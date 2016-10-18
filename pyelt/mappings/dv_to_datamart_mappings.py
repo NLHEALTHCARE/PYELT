6a57f5f58da3ec92aaf61ec1ccc1f8529c21aa70
hvreenen <henk-jan.van.reenen@nlhealthcareclinics.com>
2016-10-18 15:07:23 +0200

    datamarts samples

--- pyelt/mappings/dv_to_datamart_mappings.py
+++ pyelt/mappings/dv_to_datamart_mappings.py
@@ -1,38 +1,22 @@
-from typing import Union, List, Dict
+from pyelt.mappings.base import BaseTableMapping, FieldMapping
 
 
-from pyelt.datalayers.database import Column, Table, Database, View, Schema
-from pyelt.datalayers.dv import DvEntity, Link, HybridSat, LinkReference, Sat, DynamicLinkReference
-from pyelt.datalayers.dwh import Dwh
-from pyelt.datalayers.sor import SorTable
-from pyelt.helpers.exceptions import PyeltException
-from pyelt.mappings.base import BaseTableMapping, FieldMapping, ConstantValue
-from pyelt.mappings.transformations import FieldTransformation
+class DvToDmMapping(BaseTableMapping):
+    def __init__(self, source, target):
+        super().__init__(source, target)
 
+    def map_field(self, source: str, target: str = '', transform_func: 'FieldTransformation'=None, ref: str = '') -> None:
+        field_mapping = DvToDmFieldMapping(source, target)
+        self.field_mappings.append(field_mapping)
 
-# backup gemaakt op 21062016: "sor_to_dv_mappings_old21062016.py"
-#
-# class EntityToDimMapping(BaseTableMapping):
-#     pass
-#
-#
-# class Fact():
-#     def __init__(self, name):
-#         self.dims = []
-#         self.cols = []
-#
-#
-# class Dim():
-#     def __init__(self, name):
-#         self.cols = []
-#
-#
-# def test():
-#     fact_inschrijving = Fact('inschrijving')
-#     fact_inschrijving.dims.append(Dim('specialisatie'))
-#     fact_inschrijving.dims.append(Dim('organisatie'))
-#     fact_inschrijving.dims.append(Dim('dag_maand'))
-#     fact_inschrijving.fact = 'aantal'
-#     mapping = EntityToDimMapping(Traject, fact_inschrijving)
-#     mapping.map_fact_field('Count', fact_inschrijving.fact)
-#     mapping.map_dim(Traject.Default.start, fact_inschrijving['dag_maand'].cols['datum'])
+    def to_pygram_mapping(self, Dim):
+        d = {}
+        for field_mapping in self.field_mappings:
+            if field_mapping.target.table == Dim:
+                d[field_mapping.target.name] = field_mapping.source.name
+        return d
+
+class DvToDmFieldMapping(FieldMapping):
+    def __init__(self, source, target):
+        super().__init__(source, target)
+        self.table = target
