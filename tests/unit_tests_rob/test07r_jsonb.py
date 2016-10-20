aa242a3b6ac9712e0dd8d06066b84c3b5411ea07
RFWiegerinck <rob.wiegerinck@nlhealthcareclinics.com>
2016-10-20 15:10:32 +0200

    unittest voor json

--- tests/unit_tests_rob/test07r_jsonb.py
+++ tests/unit_tests_rob/test07r_jsonb.py
@@ -69,6 +69,21 @@ class TestCase_RunProces(unittest.TestCase):
         test_row_count(self, 'dv.patient_sat', 1)
 
 
+def test02_dv_update(self):
+        # return
+        print("test_run2:\n")
+        path = self.pipe.jsontest_config['data_path']
+        self.pipe.mappings[0].file_name = path + '/test2.csv'
+
+
+        self.pipeline.run()
+
+        test_row_count(self, 'sor_test.patient_hstage', 2)
+        test_row_count(self, 'dv.patient_hub', 2)
+        test_row_count(self, 'dv.patient_sat', 2)
+
+
+
 
 def test_row_count(unittest, table_name, count):
     test_sql = "SELECT * FROM " + table_name
@@ -114,24 +129,7 @@ def execute_sql(sql):
     return result
 
 
-def init_db():
-    engine = create_engine(test_config['conn_dwh'])
 
-    sql = """
-    DROP SCHEMA IF EXISTS sor CASCADE;
-    DROP SCHEMA IF EXISTS sor_test_system CASCADE;
-    DROP SCHEMA IF EXISTS rdv CASCADE;
-    DROP SCHEMA IF EXISTS dv CASCADE;
-    DROP SCHEMA IF EXISTS sys CASCADE;
-
-    --CREATE SCHEMA hoeft niet!! Want wordt gedaan bij aanmaken van pipeline, als schema nog niet bestaat
-    --CREATE SCHEMA sor;
-    --CREATE SCHEMA rdv;
-    --CREATE SCHEMA dv;
-    """
-
-    query = text(sql)
-    engine.execute(query)
 
 
 if __name__ == '__main__':
