7f3fc0a481bf7e1d30bd15b04029d09e85100f4e
hvreenen <henk-jan.van.reenen@nlhealthcareclinics.com>
2017-02-21 11:51:19 +0100

    proms

--- pyelt/process/etl.py
+++ pyelt/process/etl.py
@@ -448,11 +448,7 @@ AND {filter} AND {filter_runid};""".format( **params)
                 self.execute(sql, 'load in mem performance')
 
                 sql = """WITH sorquery as (SELECT {bk_mapping} as bk FROM ({sql}) as q)
-UPDATE {sor}.{sor_table} hstg SET fk_{relation_type}{hub} = hub._id FROM {dv_schema}.{hub} hub JOIN sorquery ON sorquery.bk = hub.bk AND hstg._valid AND {filter} AND {filter_runid};""".format(
-
-
-
-                    **params)
+UPDATE {sor}.{sor_table} hstg SET fk_{relation_type}{hub} = hub._id FROM {dv_schema}.{hub} hub JOIN sorquery ON sorquery.bk = hub.bk AND hstg._valid AND {filter} AND {filter_runid};""".format(**params)
                 self.execute(sql, 'update fk_hub in sor table')
 
             elif mappings.key_mappings:
