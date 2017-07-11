70cf9d3c006fd506d5cdb360ad54745957efae23
hvreenen <henk-jan.van.reenen@nlhealthcareclinics.com>
2017-07-11 10:02:38 +0200

    performance tests sor

---
+++ research/drop_sor_indexes.sql
@@ -0,0 +1,62 @@
+DROP INDEX sor_timeff.ix_afdeling_temp_hash_key;
+DROP INDEX sor_timeff.ix_afdeling_temp_hash_hash;
+DROP INDEX sor_timeff.ix_afdeling_hstage_key;
+DROP INDEX sor_timeff.ix_afdeling_hstage_hash;
+DROP INDEX sor_timeff.ix_afdeling_hstage_runid;
+
+DROP INDEX sor_timeff.ix_afspraak_deelnemer_temp_hash_key;
+DROP INDEX sor_timeff.ix_afspraak_deelnemer_temp_hash_hash;
+DROP INDEX sor_timeff.ix_afspraak_deelnemer_hstage_key;
+DROP INDEX sor_timeff.ix_afspraak_deelnemer_hstage_hash;
+DROP INDEX sor_timeff.ix_afspraak_deelnemer_hstage_runid;
+
+DROP INDEX sor_timeff.ix_contactgegevens_temp_hash_key;
+DROP INDEX sor_timeff.ix_contactgegevens_temp_hash_hash;
+DROP INDEX sor_timeff.ix_contactgegevens_hstage_key;
+DROP INDEX sor_timeff.ix_contactgegevens_hstage_hash;
+DROP INDEX sor_timeff.ix_contactgegevens_hstage_runid;
+
+DROP INDEX sor_timeff.ix_factuurregel_temp_hash_key;
+DROP INDEX sor_timeff.ix_factuurregel_temp_hash_hash;
+DROP INDEX sor_timeff.ix_factuurregel_hstage_key;
+DROP INDEX sor_timeff.ix_factuurregel_hstage_hash;
+DROP INDEX sor_timeff.ix_factuurregel_hstage_runid;
+
+DROP INDEX sor_timeff.ix_medewerker_temp_hash_key;
+DROP INDEX sor_timeff.ix_medewerker_temp_hash_hash;
+DROP INDEX sor_timeff.ix_medewerker_hstage_key;
+DROP INDEX sor_timeff.ix_medewerker_hstage_hash;
+DROP INDEX sor_timeff.ix_medewerker_hstage_runid;
+
+DROP INDEX sor_timeff.ix_persoon_temp_hash_key;
+DROP INDEX sor_timeff.ix_persoon_temp_hash_hash;
+DROP INDEX sor_timeff.ix_persoon_hstage_key;
+DROP INDEX sor_timeff.ix_persoon_hstage_hash;
+DROP INDEX sor_timeff.ix_persoon_hstage_runid;
+
+DROP INDEX sor_timeff.ix_subtraject_temp_hash_key;
+DROP INDEX sor_timeff.ix_subtraject_temp_hash_hash;
+DROP INDEX sor_timeff.ix_subtraject_hstage_key;
+DROP INDEX sor_timeff.ix_subtraject_hstage_hash;
+DROP INDEX sor_timeff.ix_subtraject_hstage_runid;
+
+DROP INDEX sor_timeff.ix_tarieven_temp_hash_key;
+DROP INDEX sor_timeff.ix_tarieven_temp_hash_hash;
+DROP INDEX sor_timeff.ix_tarieven_hstage_key;
+DROP INDEX sor_timeff.ix_tarieven_hstage_hash;
+DROP INDEX sor_timeff.ix_tarieven_hstage_runid;
+
+DROP INDEX sor_timeff.ix_vestiging_temp_hash_key;
+DROP INDEX sor_timeff.ix_vestiging_temp_hash_hash;
+DROP INDEX sor_timeff.ix_vestiging_hstage_key;
+DROP INDEX sor_timeff.ix_vestiging_hstage_hash;
+DROP INDEX sor_timeff.ix_vestiging_hstage_runid;
+
+DROP INDEX sor_timeff.ix_zorgactiviteit_temp_hash_key;
+DROP INDEX sor_timeff.ix_zorgactiviteit_temp_hash_hash;
+DROP INDEX sor_timeff.ix_zorgactiviteit_hstage_key;
+DROP INDEX sor_timeff.ix_zorgactiviteit_hstage_hash;
+DROP INDEX sor_timeff.ix_zorgactiviteit_hstage_runid;
+
+
+
--- tests/unit_tests_extra/_configs.py
+++ tests/unit_tests_extra/_configs.py
@@ -1,7 +1,7 @@
 # conn_string_test_db = 'postgresql://postgres:pgsuadmin2016@localhost:5432/pyelt_unittests'
 
 general_config = {
-    'log_path': '/logs/',
+    'log_path': '/logs/perform_tests',
     'conn_dwh': 'postgresql://postgres:pgsuadmin2016@localhost:5432/pyelt_unittests',
     'debug': False,
     'on_errors': 'throwx',
